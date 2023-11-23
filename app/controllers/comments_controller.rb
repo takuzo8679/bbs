dc6fcb8f4c2de1c579a5ef19f0917b75ceb7c0d0
takuzo <takuya.koiso.s@gmail.com>
2023-11-23 17:07:51 +0900

    add: comment保存処理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = "コメントを投稿しました"
+      redirect_to comment.board
+    else
+      flash[:comment] = comment
+      flash[:error_messages] = commnet.errors.full_messages
+      redirect_back fallback_location: comment.board
+    end
   end
 
   def destroy
   end
 end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
+  end

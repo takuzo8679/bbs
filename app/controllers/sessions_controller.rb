cba63e761c5194786353a51729309ea1b74c63d9
takuzo <takuya.koiso.s@gmail.com>
2023-11-25 15:38:46 +0900

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(name: params:[:session][:name])
+    user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to mypage_path

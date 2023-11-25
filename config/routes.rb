99590d7b346e022268e3b5fce32bf5cad370b44b
takuzo <takuya.koiso.s@gmail.com>
2023-11-25 10:41:12 +0900

    ユーザー認証に必要なファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'home/index'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

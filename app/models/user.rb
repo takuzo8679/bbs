39146283ed1d533a5ca950affa93639571ebc1e2
takuzo <takuya.koiso.s@gmail.com>
2023-11-25 14:07:05 +0900

    userモデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,17 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+    presence: ture,
+    uniquness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/
+      message: 'は小文字英数字で入力してください'
+    }
+
+  validates :password,
+    length: { minimum: 8}
 end

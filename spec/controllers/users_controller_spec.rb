240a1cc687d2729c2a072b177d95b28f61bbefe5
takuzo <takuya.koiso.s@gmail.com>
2023-11-27 06:10:19 +0900

    users createのテスト追加(異常系)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,12 +23,19 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe 'POST #create' do
-    context '正しいユーザー情報が渡ってきた場合' do
+    before do
+      # refereは現在のページの前にどこにいたかを示す情報。これがないとエラーになるので設定しておく
+        # @を付けてインスタンス変数とすることでブロックを超えて参照できる
+          # 実装がredirect_backではなくredirectでページ指定しているので今回はコメントアウト
+      # @referer = 'http://localhost'
+      # @request.env['HTTP_REFERER'] = @referer
+    end
+    context '正しいユーザー情報が渡って来た場合' do
       let(:params) do
         { user: {
-          name: 'user',
-          password: 'password',
-          password_confirmation: 'password',
+            name: 'user',
+            password: 'password',
+            password_confirmation: 'password',
           }
         }
       end
@@ -37,8 +44,32 @@ RSpec.describe UsersController, type: :controller do
           # ブロックで渡す。この場合はcountが1増えていること
         expect { post :create, params: params }.to change(User, :count).by(1)
       end
-      it 'マイページにリダイレクトされていること' do
+      it 'マイページにリダイレクトされること' do
         expect(post :create, params: params).to redirect_to(mypage_path)
+      end
+    end
+    context 'パラメータに正しいユーザー名、確認パスワードが含まれていない場合' do
+      before do
+        post(:create, params: {
+          user: {
+            name: 'ユーザー1',
+            password: 'password',
+            password_confirmation: 'invalid_password'
+          }
+        })
+      end
+      # リファラーにリダイレクトされないので省略
+      # it 'リファラーにリダイレクトされること' do
+      #   expect(response).to redirect_to(@referer)
+      # end
+      it 'リダイレクトされること' do
+        expect(response).to redirect_to('http://test.host/users/new')
+      end
+      it 'ユーザー名のエラーメッセージが含まれていること' do
+        expect(flash[:error_messages]).to include 'ユーザー名は小文字英数字で入力してください'
+      end
+      it 'パスワード確認のエラーメッセージが含まれていること' do
+        expect(flash[:error_messages]).to include 'パスワード(確認)とパスワードの入力が一致しません'
       end
     end
   end
